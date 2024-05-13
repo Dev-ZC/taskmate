d2b8c3b0dca617f6ca56d2a83762751d3a4c795b
Zaki Cole <zacole@usc.edu>
2024-05-13 18:17:44 -0400

    before navbar routing

--- frontend/management-app/src/App.js
+++ frontend/management-app/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 import Nav from './components/navbar/nav.js'
 import Landing from './components/landing/landing.js';
 
 
 function App() {
+  const [page, setPage] = useState(<Landing />)
+
+  
+
   return (
     <div className="App">
       <Nav />
-      <Landing />
+      {page}
     </div>
 
   );
